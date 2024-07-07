f025176d7a76cc8f60746982e28c86aa4b209467
Shakib Hossen <hossenmdshakib47@gmail.com>
2024-07-08 01:59:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Layer1
+const layer1 = document.querySelector(".layer");
+const wrapper = document.querySelector(".wrapper");
+const leafImg = document.querySelector(".leaf");
+const halfOrange = document.querySelector(".half-orrange");
+const portionOrange = document.querySelector(".portion-orrange");
+const labelName = document.querySelector(".toggle1");
+const toggle = document.querySelector(".toggle");
+
+const nav = document.querySelector(".navigation");
+const body = document.querySelector("body");
+
+toggle.addEventListener("change", (e) => {
+  nav.style.backgroundColor = "black";
+});
